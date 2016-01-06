3904988565053fc6a61d7428c9f102f58a7458a6
lavvsont <lavvsont@f214-11.e-im2ag.ujf-grenoble.fr>
2016-01-06 13:16:39 +0100

    inclusion de l'API

--- contenu_sections.h
+++ contenu_sections.h
@@ -1,13 +1,11 @@
-#ifndef _CONTENU_SECTIONS_
-#define _CONTENU_SECTION_
+#ifndef __CONTENU_SECTIONS__
+#define __CONTENU_SECTIONS__
 
-//Procédure d'inversion de big endian sur un Header (en tête)
-//Attention ne swap qu'une partie des valeurs
-void bytes_swap_elfHeader(Elf32_Ehdr *elfHeader);
-
-//Procédure d'inversion de big endian sur un Header de section
-//Attention ne swap qu'une partie des valeurs
-void bytes_swap_elfSections(Elf32_Shdr *elfSections)
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+#include "API.h"
+#define N 144
 
 //Fonction qui afficher à l'écran le contenu de la section une fois trouvée
 void ecrire_contenu(FILE *f,char strg[N],int size);
