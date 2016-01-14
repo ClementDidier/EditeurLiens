5481c12313fd0b11dcfdaff3dc886c301ac26394
Alixia <lilounixette@hotmail.fr>
2016-01-14 13:36:50 +0100

    Petite modification pour la partie 11

--- src/Programme_Editeur_Liens/Simulation.c
+++ src/Programme_Editeur_Liens/Simulation.c
@@ -8,8 +8,6 @@
 #include "debug.h"
 
 
-
-
 void init_simulation( arm_simulator_data_t *sim , Shdr_list *shdr_list){
 	Shdr_list *L = shdr_list;
 	*sim = arm_connect( "localhost", "6666" );
@@ -37,7 +35,7 @@ int main( int argc, char ** argv ){
 
 	//Initialisations des variables utiles
 	Elf32_Ehdr h;
-	Shdr_list shdr_list;
+	Shdr_list shdr_list, *L;
 	Sym_list  sym_list;
 	Shdr_list * rel_list = NULL;
 	int * num_sections;
@@ -58,6 +56,53 @@ int main( int argc, char ** argv ){
 	int i;
 	for( i = 0; i < size_num_sections ; i++)
 		num_sections[i] = i;
+/*
+	//Affichage du fichiers
+	char menu;
+	printf("Voulez-vous afficher les différentes structures ? o ou n \n");
+	scnaf("%c", &menu);
+	while(menu != 'o' || menu != 'n'){
+		printf("Ecrire o ou n s'il vous plaît \n");
+		scanf("%c", &menu);
+	}
+	if(menu == 'o'){
+		int boolean = 1;
+		while(boolean){
+			printf("Quelle section voulez-vous afficher ? \n");
+			printf("En-tête : h\n");
+			printf("En-tête de section : S\n");
+			printf("Table des symboles : s\n");
+			printf("Affichage des sections : x\n");
+			printf("Tout le fichier : a\n");
+			printf("Finalement, ne rien afficher : q\n")
+			scanf("%c", &menu);
+			switch(menu){
+				case 'h':
+					afficher_Elf32_Ehdr( h )
+					boolean = 0;
+					break;
+				case 'S':
+					boolean = 0;
+					break;
+				case 's':
+					boolean = 0;
+					break;
+				case 'x':
+					boolean = 0;
+					break;
+				case 'a':
+					boolean = 0;
+					break;
+				case 'q':
+					boolean = 0;
+					break;
+			}
+
+		}
+
+	}
+
+*/
 
 	// Suppression des sections de relocation 
 	printf("[+]Séparation des sections de relocation (.rel) ...\n");
@@ -77,6 +122,10 @@ int main( int argc, char ** argv ){
 	// Reimplantation
 	printf("[+]Reimplantation...\n");
 	reimplantation(h,rel_list,&shdr_list, sym_list,num_sections);
+	// Point d'entree du fichier executable
+	L = find_section_name( names, ".text", shdr_list );
+	h.e_entry = L->header.sh_addr;
+	h.e_flags |= 0x2;
 	
 	// Ecriture dans un fichier
 	if((fres = fopen("elfres.exe", "w")) == NULL)
@@ -93,17 +142,17 @@ int main( int argc, char ** argv ){
 	fclose(fres);
 	
 	printf("[-]Transformation terminee, le resultat peut etre observe via 'readelf [opt] elfres.exe' \n\n");
-	/*
+	
 	printf("[+]Simulation... \n");
 	init_simulation( &sim, &shdr_list );
 	
 	//arm_run( sim );
-	
+	/*
 	while( getc( stdin ) != '0' )
 		arm_step( sim );
-	*/
+	//
 	//liberation des variables utilisées
-
+*/
 	liberer_Shdr_list(&shdr_list);
 	liberer_Shdr_list(rel_list);
 	liberer_Sym_list(&sym_list);
